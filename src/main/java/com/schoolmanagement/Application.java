e1afc26c36600f7ea4cf5e2981a77ed1535b7a3b
Tushar Pandey <tusharpandey586@gmail.com>
2024-03-07 15:06:28 +0530

    Resolved cors error

--- src/main/java/com/schoolmanagement/Application.java
+++ src/main/java/com/schoolmanagement/Application.java
@@ -15,19 +15,21 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		
 	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer()
-	{
-		return new WebMvcConfigurer()
-		{
-			@Override
-			public void addCorsMappings(CorsRegistry registry)
-			{
-				registry.addMapping("/*").allowedOrigins("http://localhost:5173");
-			}
-				
-		};
-	}
+//	
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer()
+//	{
+//		return new WebMvcConfigurer()
+//		{
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry)
+//			{
+//				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+//			}
+//				
+//		};
+//	}
+//	
+
 
 }
--- src/main/java/com/schoolmanagement/config/SecurityConfig.java
+++ src/main/java/com/schoolmanagement/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.schoolmanagement.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,15 +9,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.schoolmanagement.security.JwtAuthenticationEntryPoint;
 import com.schoolmanagement.security.JwtAuthenticationFilter;
-
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -31,8 +37,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.csrf(csrf -> csrf.disable())
-		.cors(cors->cors.disable())
+		http.cors().and().csrf(csrf -> csrf.disable())
 		.authorizeHttpRequests(auth->auth.requestMatchers("/admin/**").hasRole("ADMIN")
 				.requestMatchers("/student/**").hasRole("STUDENT")
 				.requestMatchers("/auth/login").permitAll()
@@ -41,6 +46,7 @@ public class SecurityConfig {
 				.anyRequest().authenticated())
 				.exceptionHandling(ex -> ex.authenticationEntryPoint(point))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
 		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
@@ -60,5 +66,16 @@ public class SecurityConfig {
 
 	}
 
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
