312f49ba5daa1b9a3233af9695176de6423e902f
Tushar Pandey <tusharpandey586@gmail.com>
2024-04-15 20:31:54 +0530

    added pass

--- src/main/java/com/schoolmanagement/entities/SchoolVO.java
+++ src/main/java/com/schoolmanagement/entities/SchoolVO.java
@@ -36,6 +36,8 @@ public class SchoolVO {
 	private String schRegNo;
 	@Column(name="SCH_EMAIL",nullable=false)
 	private String email;
+	@Column(name="SCH_PASS")
+	private String password;
 
 	@Column(name="SCH_ADD")
 	private String schAddress;
